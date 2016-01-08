554eb646b306acde326d76655657d5f3fb3ba936
fenixlin <fenixlin@yandex.com>
2016-01-09 00:06:28 +0800

    db.py: add negative when dumping, and fix bug of non-binary dumping

--- sample_exp.py
+++ sample_exp.py
@@ -7,16 +7,15 @@ def sample_exp():
     db = Database('movielens', 718, 8928, 'ratings.csv', 'movies.attr')
     db.load_data()
     db.make_train_test_matrix()
-    db.add_negative_data('train')
-    db.add_negative_data('test',addAllUsers=True)
-    db.dump_libfm_data('train_step1.libfm', 'test_step1.libfm')
-
     libfm = LibFM()
+
     # step 1: classification to predict interest
+    db.dump_libfm_data('train_step1.libfm', 'test_step1.libfm', add_negative=True, binary=True)
     libfm.run('c', 'train_step1.libfm', 'test_step1.libfm', 'pred.libfm')
     step1_pred_list = db.load_pred_list('pred.libfm', 'c')
 
     # step 2: regression to predict possible rating
+    db.dump_libfm_data('train_step2.libfm', 'test_step2.libfm', add_negative=False, binary=False)
     libfm.run('r', 'train_step1.libfm', 'test_step1.libfm', 'pred.libfm')
     step2_pred_list = db.load_pred_list('pred.libfm', 'r')
 
